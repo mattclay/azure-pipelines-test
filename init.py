2eb8ee33dc93b8d412e00ee5521540bc693574b4
Matt Clay <matt@mystile.com>
2019-10-10 14:06:47 -0700

    Add more output.

--- init.py
+++ init.py
@@ -11,6 +11,9 @@ config = dict(
         name='B',
     ),
 )
+var_value = json.dumps(config)
 
 # see https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands?view=azure-devops&tabs=bash
-print('##vso[task.setVariable variable=%s;isOutput=true]%s' % (var_name, json.dumps(config)))
+print('Setting variable: %s' % var_name)
+print('##vso[task.setVariable variable=%s;isOutput=true]%s' % (var_name, var_value))
+print('Variable has been set to: %s' % var_value)
