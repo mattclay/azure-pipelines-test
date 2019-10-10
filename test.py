c7ddfe3b6bdef9e450fbcd04d2e7873df257d5e3
Matt Clay <matt@mystile.com>
2019-10-10 16:46:06 -0700

    Add matrix. (#1)

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+name = sys.argv[1]
+
+print('Hello my job name is: %s' % name)
+
+# if name == 'Test #001':
+#     print('Fail')
+#     sys.exit(1)
+
+print('Pass')
