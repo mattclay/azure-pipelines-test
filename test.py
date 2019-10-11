bba03edb44dea9ccf8bf23e15a621f3dedc5389c
Matt Clay <matt@mystile.com>
2019-10-10 18:07:17 -0700

    Show incremental progress.

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import sys
+import time
 
 name = sys.argv[1]
 
 print('Hello my job name is: %s' % name)
+
+for i in range(1, 10):
+    print('Sleeping for %d second(s)' % i)
+    time.sleep(i)
 
 if name == 'Test #001':
     print('Fail')
