6e4893e353018049845e3663ab4bd12d3093672e
Matt Clay <matt@mystile.com>
2020-10-02 13:06:08 -0700

    Static matrix that fails on "Four".

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-import random
 
 name = sys.argv[1]
 
 print('Hello my job name is: %s' % name)
 
-if name == 'Job #002':
-    print('Job "%s" should not run.')
-    sys.exit(1)
-
-if random.randint(1, 3) == 1:
+if name == 'Four':
     print('Fail')
-    sys.exit(1)  # intentional failure
+    sys.exit(1)
 
 print('Pass')
