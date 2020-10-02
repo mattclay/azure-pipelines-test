43c4c0423b72fde43b96619422da43898e83d221
Matt Clay <matt@mystile.com>
2020-10-02 13:00:50 -0700

    Complain about Job #002.

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ name = sys.argv[1]
 
 print('Hello my job name is: %s' % name)
 
+if name == 'Job #002':
+    print('Job "%s" should not run.')
+    sys.exit(1)
+
 if random.randint(1, 3) == 1:
     print('Fail')
     sys.exit(1)  # intentional failure
