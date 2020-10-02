78d8a2e10d8102b8d73edc60865199bf82be5ac6
Matt Clay <matt@mystile.com>
2020-10-02 13:07:15 -0700

    Add more failures.

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ name = sys.argv[1]
 
 print('Hello my job name is: %s' % name)
 
+if name == 'Two':
+    print('Fail')
+    sys.exit(1)
+
+if name == 'Three':
+    print('Fail')
+    sys.exit(1)
+
 if name == 'Four':
     print('Fail')
     sys.exit(1)
